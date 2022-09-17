c9cfe48497a332eec4af1ac9be45f6aca20081fd
MiaoC <201930384059@mail.scut.edu.cn>
2022-09-17 12:43:42 +0800

    修改了内存初始化的bug，现在代码可以正常运行

--- src/main.js
+++ src/main.js
@@ -20,13 +20,13 @@ import "./global";
 import "./room";
 
 // 全局重启的时候需要重新初始化StructureIndex和Memory
-Memory.doNotInitializeMyStructureIndex = false;
-Memory.doNotInitializeMyMemory = false;
+// Memory.doNotInitializeMyStructureIndex = false;
+// Memory.doNotInitializeMyMemory = false;
+RawMemory.set("{}");
 
 // 主循环，每个tick调用
 module.exports.loop = errorMapper(() => {
     if (!Memory.doNotClearMyMemory) {
-        RawMemory.set("{}");
         Memory.doNotClearMyMemory = true;
         console.log("tick：" + Game.time + '清空了内存，下一tick将开始内存初始化');
         return undefined;
