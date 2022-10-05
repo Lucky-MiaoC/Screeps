031bd35ac642c9bb7e1511cea463f2cf852c83bd
MiaoC <201930384059@mail.scut.edu.cn>
2022-10-05 16:24:44 +0800

    修复了nuker的小bug

--- src/structures/nuker.js
+++ src/structures/nuker.js
@@ -11,8 +11,8 @@ StructureNuker.prototype.testLaunchNuke = function (pos) {
     console.log(`测试冷却时间...${this.cooldown == 0 ? '通过！' : (flag = 1, '未通过！')}`);
     console.log(`测试ENERGY...${this.store.getFreeCapacity(RESOURCE_ENERGY) == 0 ? '通过！' : (flag = 1, '未通过！')}`);
     console.log(`测试GHODIUM...${this.store.getFreeCapacity(RESOURCE_GHODIUM) == 0 ? '通过！' : (flag = 1, '未通过！')}`);
-    console.log(`测试发射距离...${Game.map.getRoomLinearDistance(this.room.name, pos.roomName) <= 10 ? '通过！' : (flag = 1, '未通过！')}`);
-    console.log(`测试目标房间开发状态...${Game.map.getRoomStatus(pos.roomName).status == 'normal' ? '通过！' : (flag = 1, '未通过！')}`);
+    console.log(`测试发射距离...${Game.map.getRoomLinearDistance(this.room.name, _pos.roomName) <= 10 ? '通过！' : (flag = 1, '未通过！')}`);
+    console.log(`测试目标房间开发状态...${Game.map.getRoomStatus(_pos.roomName).status == 'normal' ? '通过！' : (flag = 1, '未通过！')}`);
     return flag ? '未通过测试！' : '通过测试！';
 }
 
